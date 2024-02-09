1db9ae61440aadbbda06cef8ba1894a4e66f58ce
niket priyadarshi <niket.priyadarshi@secninjaz.com>
2024-02-09 16:53:39 +0530

    Initial Commit

---
+++ expandableButton/expandableButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  expandableButtonApp.swift
+//  expandableButton
+//
+//  Created by Manu Singh on 09/02/24.
+//
+
+import SwiftUI
+
+@main
+struct expandableButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
